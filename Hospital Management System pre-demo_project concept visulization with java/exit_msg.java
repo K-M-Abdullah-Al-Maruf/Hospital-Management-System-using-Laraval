df2c52672588ffbd28b9978b800bd3fba981fc15
K.M. Abdullah Al Maruf <marufkhan600gm@gmail.com>
2022-08-18 05:23:39 +0600

    Second commit

--- Hospital Management System pre-demo_project concept visulization with java/exit_msg.java
+++
@@ -1,9 +0,0 @@
-package com.Hospital;
-
-public class exit_msg {
-    public exit_msg()
-    {
-        System.out.println("                                                     THANK YOU VERY MUCH FOR CHOOSING OUR SERVICE                                                    ");
-        System.out.println("                                                                PLEASE VISIT US AGAIN                                                                ");
-    }
-}
